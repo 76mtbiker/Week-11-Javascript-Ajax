6c03507a8b2085ffd8fb91e99ba68ff6844c8fe8
76mtbiker <jcribbins@gmail.com>
2023-04-23 17:20:43 -0600

    pushing week 11

---
+++ assignment.js
@@ -0,0 +1,47 @@
+// let paragraph = document.getElementById('text');//
+// console.log(paragraph);
+
+// paragraph.innerHTML = 'Howdy Do Dah';
+// console.log(paragraph);
+
+// function sayHello(name,lname,age,hometown){
+//     console.log('hello '+name+' ' +lname+' ' +age+' '+hometown)//add as many paramaters as want
+    
+// }
+// sayHello('bernardo','rosario','34','NewYork')
+// sayHello('billy')
+// sayHello('jerry')
+// sayHello('Maggy')
+
+function sayFuck(name,age,bike){
+    console.log('Fuck  '+name+' ' +age+' '+bike)
+    
+
+}
+sayFuck('jerry','47')
+sayFuck('jerry','76','yeti')
+
+// let p = $('#test');
+// let div = $('.my-class');
+// let ul = $('ul');
+
+// console.log(p);
+// console.log(div);
+// console.log(ul);
+
+// console.log(p.text());
+// p.text('New Text')
+
+// // $('input').val('New value');
+// $('input').attr('placeholder', 'Placeholder Text');
+
+// div.prepend('<p>prepended paragraph');
+// div.append('<p>appended paragraph</p>');
+// div.before('<p>paragraph that was added before the div</p>');
+// div.after('<p>paragraph added after the div </p>');
+
+// div.empty();
+// ul.remove();
+
+// $('input').hide();
+// setTimeout(() => $('input').show(), 2000);
